2c817690190eeea16a95ba78a9142e35a6702ca1
Sean Chen <opjlmi@gmail.com>
2016-10-07 15:00:14 +0800

    Fix typo

--- vue-src/src/main.js
+++ vue-src/src/main.js
@@ -1,10 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => { return h(App) },
+  render: (h) => { return h(App) },
 })
