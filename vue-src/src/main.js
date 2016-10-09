4c95f768153c20a8c8f9ce452f6a24fd7a6ec0da
Sean Chen <opjlmi@gmail.com>
2016-10-09 15:07:04 +0800

    Create module file to split Vuex config

--- vue-src/src/main.js
+++ vue-src/src/main.js
@@ -1,90 +1,10 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// Import module
 import Vue from 'vue'
-import Vuex from 'vuex'
-import moment from 'moment'
-import App from './App.vue'
 
-Vue.use(Vuex)
-const store = new Vuex.Store({
-  state: {
-    // timestamp
-    timestamp: null,
-    // dblist
-    dblist: [],
-    // res
-    status: null,
-    msg: null,
-    // siteinfo
-    siteinfo: {},
-    // res.data
-    sitename: null,
-    count: null,
-    zc: null,
-    rawdata: null,
-    sourcerms: null,
-    dfs: null,
-    rms: null,
-    fft: null,
-    // res.data.condition
-    day: null,
-    time: null,
-    temp: null,
-    hum: null,
-    lon: null,
-    lat: null,
-  },
-  mutations: {
-    DBLIST(state, dblist) {
-      state.dblist = dblist
-    },
-    SITEDATA(state, res) {
-      // res
-      state.status = res.status
-      state.msg = res.msg
-      // res.data
-      state.sitename = res.data.sitename
-      state.count = res.data.count
-      state.zc = res.data.zc
-      state.rawdata = res.data.rawdata
-      state.sourcerms = res.data.sourcerms
-      state.dfs = res.data.dfs
-      state.rms = res.data.rms
-      state.fft = res.data.fft
-      // res.data.condition
-      state.day = moment(res.data.condition.time).format('YYYY/MM/DD')
-      state.time = moment(res.data.condition.time).format('HH:mm:ss')
-      state.temp = res.data.condition.temp
-      state.hum = res.data.condition.hum
-      state.lon = res.data.condition.lon
-      state.lat = res.data.condition.lat
-      state.timestamp = Date.now()
-    },
-  },
-  getters: {
-    fft: (state) => {
-      return state.fft
-    },
-    timestamp: (state) => {
-      return state.timestamp
-    },
-    rawdata: (state) => {
-      return state.rawdata
-    },
-    sourcerms: (state) => {
-      return state.sourcerms
-    },
-    dfs: (state) => {
-      return state.dfs
-    },
-    rms: (state) => {
-      return state.rms
-    },
-    zc: (state) => {
-      return state.zc
-    },
-  },
-})
+// Import Script
+import App from './App.vue'
+import store from './vuex/store'
+
 
 /* eslint-disable no-new */
 new Vue({
