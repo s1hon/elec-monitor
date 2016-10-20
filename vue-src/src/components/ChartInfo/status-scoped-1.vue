7bae44763742c058dc581de187cf744ee0e439f6
Sean Chen <opjlmi@gmail.com>
2016-10-20 21:22:27 +0800

    REMOVE scope annotation

--- vue-src/src/components/ChartInfo/status-scoped-1.vue
+++ vue-src/src/components/ChartInfo/status-scoped-1.vue
@@ -34,18 +34,5 @@ export default {
     float: left;
     width: 80px;
   }
-  // background-color: black;
-  // margin-top: 35px;
-  // padding: 0 0 5px 80px;
-
-  // @media screen and (max-width: 720px) {
-  //   float: left;
-  //
-  //   div {
-  //     // float: left;
-  //     padding: 5px 15px 0px 5px;
-  //   }
-  //   padding: 0px 0 5px 15px;
-  // }
 }
 </style>
--- vue-src/src/components/ChartInfo/status-scoped-2.vue
+++ vue-src/src/components/ChartInfo/status-scoped-2.vue
@@ -13,9 +13,6 @@
 <script>
 export default {
   computed: {
-    // sitename() {
-    //   return this.$store.state.sitename
-    // },
     harmonic() {
       return this.$store.getters.harmonic
     },
@@ -38,23 +35,5 @@ export default {
     float: left;
     width: 210px;
   }
-  // width: 400px;
-  // background-color: black;
-  // margin-top: 35px;
-  // padding: 0 0 5px 15px;
-
-  // @media screen and (max-width: 720px) {
-  //   // float: left;
-  //
-  //   div {
-  //     // float: left;
-  //     padding: 5px 15px 0px 5px;
-  //   }
-  //   padding: 0px 0 5px 15px;
-  // }
-
-  // @media screen and (max-width: 1200px) {
-  //   margin-top: 35px;
-  // }
 }
 </style>
